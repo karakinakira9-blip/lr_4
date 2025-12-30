8bc77d1b2a0c82c9da1e99e3e51c725470c58cea
karakinakira9-blip <karakinakira9@icloud.com>
2025-12-30 16:55:58 +0300

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,36 @@
+from pprint import pprint
+
+
+def toml_parser(toml_file):
+    parsed_data = {}
+    current_section = None
+    with open(toml_file, "r") as toml_file:
+        for line in toml_file:
+            cleaned_line = line.strip()
+            if not cleaned_line or cleaned_line.startswith("#"):
+                continue
+            if cleaned_line.startswith("[") and cleaned_line.endswith("]"):
+                current_section = cleaned_line[1:-1]
+                parsed_data[current_section] = {}
+            elif current_section and '=' in cleaned_line:
+                key, value = cleaned_line.split("=", 1)
+                parsed_data[current_section][key.strip()] = value.strip()[1:-1]
+    return parsed_data
+
+
+def xml_serializer(binary_object):
+    print("<?xml version='1.0' encoding='UTF-8'?>\n<xml>")
+    for section, keys_values in binary_object.items():
+        print(f"    <{section}>")
+        for key, vanlue in keys_values.items():
+            print(f"        <{key}>{vanlue}</{key}>")
+        print(f'    </{section}>')
+
+    print('</xml>')
+
+
+
+if __name__ == "__main__":
+    binary_object = toml_parser('task1.toml')
+    pprint(binary_object)
+    xml_serializer(binary_object)
